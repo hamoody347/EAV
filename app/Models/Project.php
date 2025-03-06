2b1f07cefb587e861ecc9438269f7ec542881974
Ahmed Abdelsalam <hamoody347@gmail.com>
2025-03-06 06:16:52 +0200

    Added Filterable Trait

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Filterable;
 
 class Project extends Model
 {
-
-    use HasFactory;
+    use HasFactory, Filterable;
 
     protected $fillable = [
         'name',
-        'status'
+        'status',
     ];
 
     public function users()
@@ -29,4 +29,12 @@ class Project extends Model
     {
         return $this->morphMany(AttributeValue::class, 'entity');
     }
+
+    // Define additional regular attributes that can be filtered
+    protected function getRegularAttributes()
+    {
+        return ['name', 'status'];
+    }
+
+    //For Projects: /api/projects?filters[name]=ProjectA&filters[status]=active
 }
